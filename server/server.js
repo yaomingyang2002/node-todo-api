71c2a96c557948645acb5ff63e80effeb8d3be6a
LAPTOP-URK2D9SP\YM <yaomingyang2002@yahoo.com>
2017-09-28 14:35:07 -0400

    Test POST/todos route

--- server/server.js
+++ server/server.js
@@ -25,4 +25,5 @@ app.post ('/todos', (req, res)=>{
 app.listen (3000, () => {
     console.log('Started up on port 3000');
 });
-;
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,56 @@
+const expect = require ('expect');
+const  request = require ('supertest');
+
+const  {app}  = require ('./../server');
+const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+
+//to clear Todo DB before test
+beforeEach ((done)=>{
+    Todo.remove({}).then(() => done());
+});
+
+describe('POST/todos', () =>{
+    it('should create a new todo', (done) =>{
+        let text = 'Test todo text';
+
+        request (app)
+            .post ('/todos')
+            .send({text})
+            .expect (200)
+            .expect((res) =>{
+                expect(res.body.text).toBe(text );
+        })
+        .end((err, res) => {
+            if(err){
+                return done(err);
+            }
+
+            Todo.find().then((todos) =>{
+                expect(todos.length).toBe(1);
+                expect(todos[0].text).toBe(text);
+                done();
+             }).catch((e) => done(e));
+        });
+    });
+
+    //2nd case: it will not create when bad data sent
+    it('should not create a new todo with invalid body data', (done) =>{
+        let text = ' ';
+
+        request (app)
+            .post ('/todos')
+            .send({text})
+            .expect (400)
+            .end((err, res) => {
+                if(err){
+                    return done(err);
+                }
+
+                Todo.find().then((todos) =>{
+                    expect(todos.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+        });
+});
